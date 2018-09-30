2f81de7aa5687a3138b3c1828e4d905f95a8f4a7
Juan Felipe Torres <ferchotp@Juans-MacBook-Pro-3.local>
2018-09-30 11:26:38 -0500

    Se implementa la clase SQLCliente

--- src/main/java/uniandes/isis2304/superandes/persistencia/SQLCliente.java
+++ src/main/java/uniandes/isis2304/superandes/persistencia/SQLCliente.java
@@ -1,7 +1,11 @@
 package uniandes.isis2304.superandes.persistencia;
 
+import java.util.List;
+
 import javax.jdo.PersistenceManager;
 import javax.jdo.Query;
+
+import uniandes.isis2304.superandes.negocio.Cliente;
 
 /**
  * Clase que encapsula los métodos que hacen acceso a la base de datos para el concepto CLIENTE de SuperAndes
@@ -57,5 +61,88 @@ class SQLCliente {
         q.setParameters(idCliente, nombre, correo, tipo, direccion);
         return (long) q.executeUnique();
 	}
-
+	
+	/**
+	 * Crea y ejecuta la sentencia SQL para eliminar CLIENTES de la base de datos de Superandes, por su nombre
+	 * @param pm - El manejador de persistencia
+	 * @param nombreCliente - El nombre del cliente
+	 * @return EL número de tuplas eliminadas
+	 */
+	public long eliminarClientesPorNombre (PersistenceManager pm, String nombreCliente)
+	{
+        Query q = pm.newQuery(SQL, "DELETE FROM " + pp.darTablaCliente () + " WHERE nombre = ?");
+        q.setParameters(nombreCliente);
+        return (long) q.executeUnique();
+	}
+	
+	/**
+	 * Crea y ejecuta la sentencia SQL para eliminar UN CLIENTE de la base de datos de Superandes, por su identificador
+	 * @param pm - El manejador de persistencia
+	 * @param idCliente - El identificador del cliente
+	 * @return EL número de tuplas eliminadas
+	 */
+	public long eliminarClientePorId (PersistenceManager pm, long idCliente)
+	{
+        Query q = pm.newQuery(SQL, "DELETE FROM " + pp.darTablaCliente () + " WHERE id = ?");
+        q.setParameters(idCliente);
+        return (long) q.executeUnique();
+	}
+	
+	/**
+	 * Crea y ejecuta la sentencia SQL para encontrar la información de UN CLIENTE de la 
+	 * base de datos de Superandes, por su identificador
+	 * @param pm - El manejador de persistencia
+	 * @param idCliente - El identificador del cliente
+	 * @return El objeto CLIENTE que tiene el identificador dado
+	 */
+	public Cliente darClientePorId (PersistenceManager pm, long idCliente) 
+	{
+		Query q = pm.newQuery(SQL, "SELECT * FROM " + pp.darTablaCliente () + " WHERE id = ?");
+		q.setResultClass(Cliente.class);
+		q.setParameters(idCliente);
+		return (Cliente) q.executeUnique();
+	}
+	
+	/**
+	 * Crea y ejecuta la sentencia SQL para encontrar la información de LOS CLIENTES de la 
+	 * base de datos de Superandes, por su nombre
+	 * @param pm - El manejador de persistencia
+	 * @param nombreCliente - El nombre de cliente buscado
+	 * @return Una lista de objetos CLIENTE que tienen el nombre dado
+	 */
+	public List<Cliente> darClientesPorNombre (PersistenceManager pm, String nombreCliente) 
+	{
+		Query q = pm.newQuery(SQL, "SELECT * FROM " + pp.darTablaCliente() + " WHERE nombre = ?");
+		q.setResultClass(Cliente.class);
+		q.setParameters(nombreCliente);
+		return (List<Cliente>) q.executeList();
+	}
+	
+	/**
+	 * Crea y ejecuta la sentencia SQL para encontrar la información de LOS CLIENTES de la 
+	 * base de datos de Superandes
+	 * @param pm - El manejador de persistencia
+	 * @return Una lista de objetos CLIENTE
+	 */
+	public List<Cliente> darClientes (PersistenceManager pm)
+	{
+		Query q = pm.newQuery(SQL, "SELECT * FROM " + pp.darTablaCliente());
+		q.setResultClass(Cliente.class);
+		return (List<Cliente>) q.executeList();
+	}
+	
+	/**
+	 * Crea y ejecuta la sentencia SQL para encontrar la información de LOS CLIENTES de la 
+	 * base de datos de Superandes, por su tipo
+	 * @param pm - El manejador de persistencia
+	 * @param tipoCliente - El tipo del cliente buscado
+	 * @return Una lista de objetos CLIENTE que tienen el tipo dado
+	 */
+	public List<Cliente> darClientesPorTipo (PersistenceManager pm, String tipoCliente) 
+	{
+		Query q = pm.newQuery(SQL, "SELECT * FROM " + pp.darTablaCliente() + " WHERE tipo = ?");
+		q.setResultClass(Cliente.class);
+		q.setParameters(tipoCliente);
+		return (List<Cliente>) q.executeList();
+	}
 }
