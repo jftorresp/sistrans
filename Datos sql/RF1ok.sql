09965eaf6e4b4e09832fba21f170a3b98e2b76a7
unknown <n.cobos@uniandes.edu.co>
2018-09-29 20:09:25 -0500

    reqs

---
+++ Datos sql/RF1ok.sql
@@ -0,0 +1,9 @@
+INSERT INTO A_PROVEEDOR (nit, nombre, calificacion) VALUES (2,'Super Ricas', 3);
+INSERT INTO A_PROVEEDOR (nit, nombre, calificacion) VALUES (3, 'Colanta', 6);
+INSERT INTO A_PROVEEDOR (nit, nombre, calificacion) VALUES (4, 'Ariel', 3);
+INSERT INTO A_PROVEEDOR (nit, nombre, calificacion) VALUES (5, 'Alpina', 5);
+INSERT INTO A_PROVEEDOR (nit, nombre, calificacion) VALUES (6, 'Tropicana',9 );
+
+SELECT * FROM A_PROVEEDOR;
+
+
