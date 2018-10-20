6e7190e368711615603b3703a32d7c9ef4c32508
Nicolás <n.cobos@uniandes.edu.co>
2018-10-20 00:34:33 -0500

    inicio iteracion2

---
+++ doc/Iteracion 1/Datos sql/RF5ok.sql
@@ -0,0 +1,15 @@
+INSERT INTO A_ALMACENAMIENTO
+(id, capacidadvolumen, capacidadpeso, producto, sucursal, nivelabastecimientobodega, existencias, capacidadproductos, tipo)
+VALUES (1,2000,7500,1,2,0,0,200,'Bodega');
+
+INSERT INTO A_ALMACENAMIENTO
+(id, capacidadvolumen, capacidadpeso, producto, sucursal, nivelabastecimientobodega, existencias, capacidadproductos, tipo)
+VALUES (2,2000,6500,3,2,0,0,300,'Bodega');
+
+INSERT INTO A_ALMACENAMIENTO
+(id, capacidadvolumen, capacidadpeso, producto, sucursal, nivelabastecimientobodega, existencias, capacidadproductos, tipo)
+VALUES (3,2000,7000,5,3,0,0,240,'Bodega');
+
+INSERT INTO A_ALMACENAMIENTO
+(id, capacidadvolumen, capacidadpeso, producto, sucursal, nivelabastecimientobodega, existencias, capacidadproductos, tipo)
+VALUES (4,2500,7000,8,3,0,0,210,'Bodega');
---
+++ doc/Iteracion 1/Datos sql/RF6ok.sql
@@ -0,0 +1,15 @@
+INSERT INTO A_ALMACENAMIENTO
+(id, capacidadvolumen, capacidadpeso, producto, sucursal, nivelabastecimientobodega, existencias, capacidadproductos, tipo)
+VALUES (1,200,120,5,2,10,52,80,'Estante');
+
+INSERT INTO A_ALMACENAMIENTO
+(id, capacidadvolumen, capacidadpeso, producto, sucursal, nivelabastecimientobodega, existencias, capacidadproductos, tipo)
+VALUES (2, 220,120,2,2,10,57,81,'Estante');
+
+INSERT INTO A_ALMACENAMIENTO
+(id, capacidadvolumen, capacidadpeso, producto, sucursal, nivelabastecimientobodega, existencias, capacidadproductos, tipo)
+VALUES (3, 210,120,3,3,10,55,70,'Estante');
+
+INSERT INTO A_ALMACENAMIENTO
+(id, capacidadvolumen, capacidadpeso, producto, sucursal, nivelabastecimientobodega, existencias, capacidadproductos, tipo)
+VALUES (4, 190,120,7,3,10,60,73,'Estante');
